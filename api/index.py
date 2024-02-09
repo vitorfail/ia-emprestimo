59c780b9150fc6348173edbdadd7a6088904fa75
vitorfail <vitor_andrademanoel@hotmail.com>
2024-02-09 16:28:51 -0300

    tentativa final

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from sklearn.ensemble import RandomForestClassifier
 import joblib
 import pandas as pd
 import numpy as np
+
 import os
 
 app = Flask(__name__)
